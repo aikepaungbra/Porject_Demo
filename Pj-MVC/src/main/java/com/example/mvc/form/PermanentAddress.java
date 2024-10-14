ba267a1a64109ac2d5ac676b1ad5ca6602d4a011
TayZarTun6631503089 <156641441+TayZarTun6631503089@users.noreply.github.com>
2024-10-14 09:06:53 +0000

    kit_third

--- Pj-MVC/src/main/java/com/example/mvc/form/PermanentAddress.java
+++ Pj-MVC/src/main/java/com/example/mvc/form/PermanentAddress.java
@@ -1,5 +1,64 @@
 package com.example.mvc.form;
 
 public class PermanentAddress {
+    
+    private String city_town_village;
+    private String houseNum;
+    private String moo;
+    private String road;
+    private String subDistrict;
+    private String district;
+    private String province;
+    private int postalCode;
+    
+    public String getCity_town_village() {
+        return city_town_village;
+    }
+    public void setCity_town_village(String city_town_village) {
+        this.city_town_village = city_town_village;
+    }
+    public String getHouseNum() {
+        return houseNum;
+    }
+    public void setHouseNum(String houseNum) {
+        this.houseNum = houseNum;
+    }
+    public String getMoo() {
+        return moo;
+    }
+    public void setMoo(String moo) {
+        this.moo = moo;
+    }
+    public String getRoad() {
+        return road;
+    }
+    public void setRoad(String road) {
+        this.road = road;
+    }
+    public String getSubDistrict() {
+        return subDistrict;
+    }
+    public void setSubDistrict(String subDistrict) {
+        this.subDistrict = subDistrict;
+    }
+    public String getDistrict() {
+        return district;
+    }
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+    public String getProvince() {
+        return province;
+    }
+    public void setProvince(String province) {
+        this.province = province;
+    }
+    public int getPostalCode() {
+        return postalCode;
+    }
+    public void setPostalCode(int postalCode) {
+        this.postalCode = postalCode;
+    }
+   
 
 }
